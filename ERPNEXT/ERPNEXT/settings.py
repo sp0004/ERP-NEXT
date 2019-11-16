ae8382c335ebbffd01c941fd7d6e551b71f8e138
sp00044 <32140657+sp00044@users.noreply.github.com>
2019-11-16 12:47:22 -0500

    Adding view

--- ERPNEXT/ERPNEXT/settings.py
+++ ERPNEXT/ERPNEXT/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'u_v8p1@8^0fz&$oscz-*!7h4y^%2b*rgp0enl-rh%j6kkle$9l'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ERPNEXT.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -82,10 +79,9 @@ DATABASES = {
         'PASSWORD': 'pole123',
         'HOST': 'localhost',
         'PORT': '3306'
-        
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -105,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -119,8 +114,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
--- ERPNEXT/ErpDev/views.py
+++ ERPNEXT/ErpDev/views.py
@@ -1,2 +1,5 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+def Home(request):
+    return render(request, 'home.html')
